eb03c9ec6e915bf55c94cf115fa51f6c00ae58db
Wesley McClure <wes.mcclure@gmail.com>
2013-03-21 16:50:31 -0500

    documentation updates

--- Rakefile.rb
+++ Rakefile.rb
@@ -5,11 +5,9 @@ $LOAD_PATH << File.dirname(__FILE__) + "/lib"
 require 'kindle_highlights'
 
 task :test_parse_book do
-
   contents = File.read('your_reading.html')
   KindleHighlightBackup::KindleParser.parseBooks(contents)
   .each { |asin, book| puts "#{asin} #{book.author} #{book.title}" }
-
 end
 
 task :test_save_book do
